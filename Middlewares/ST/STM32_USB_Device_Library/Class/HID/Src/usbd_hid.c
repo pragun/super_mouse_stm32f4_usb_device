6a87b6d3ef668a568aed8cd11b877f232a9f775b
Pragun <pragun.goyal@gmail.com>
2020-06-11 20:33:04 -0400

    Added vendor specific collections with their own Report Ids to the report descriptor

--- Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c
+++ Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c
@@ -315,58 +315,6 @@ __ALIGN_BEGIN static uint8_t USBD_HID_DeviceQualifierDesc[USB_LEN_DEV_QUALIFIER_
   0x00,
 };
 
-/**
- * This one is 74bytes long
-__ALIGN_BEGIN static uint8_t HID_MOUSE_ReportDesc[HID_MOUSE_REPORT_DESC_SIZE]  __ALIGN_END =
-{
-  0x05,   0x01,
-  0x09,   0x02,
-  0xA1,   0x01,
-  0x09,   0x01,
-
-  0xA1,   0x00,
-  0x05,   0x09,
-  0x19,   0x01,
-  0x29,   0x03,
-
-  0x15,   0x00,
-  0x25,   0x01,
-  0x95,   0x03,
-  0x75,   0x01,
-
-  0x81,   0x02,
-  0x95,   0x01,
-  0x75,   0x05,
-  0x81,   0x01,
-
-  0x05,   0x01,
-  0x09,   0x30,
-  0x09,   0x31,
-  0x09,   0x38,
-
-  0x15,   0x81,
-  0x25,   0x7F,
-  0x75,   0x08,
-  0x95,   0x03,
-
-  0x81,   0x06,
-  0xC0,   0x09,
-  0x3c,   0x05,
-  0xff,   0x09,
-
-  0x01,   0x15,
-  0x00,   0x25,
-  0x01,   0x75,
-  0x01,   0x95,
-
-  0x02,   0xb1,
-  0x22,   0x75,
-  0x06,   0x95,
-  0x01,   0xb1,
-
-  0x01,   0xc0
-};
-**/
 
 /*
 __ALIGN_BEGIN static uint8_t HID_MOUSE_ReportDesc[HID_MOUSE_REPORT_DESC_SIZE]  __ALIGN_END = {
@@ -425,37 +373,6 @@ __ALIGN_BEGIN static uint8_t HID_MOUSE_ReportDesc[HID_MOUSE_REPORT_DESC_SIZE]  _
 	    0xc0                           // END_COLLECTION
 }; */
 
-/*
-__ALIGN_BEGIN static uint8_t HID_MOUSE_ReportDesc[HID_MOUSE_REPORT_DESC_SIZE]  __ALIGN_END = {
-	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-	0x09, 0x02,                    // USAGE (Mouse)
-	0xa1, 0x01,                    // COLLECTION (Application)
-	0x09, 0x01,                    //   USAGE (Pointer)
-	0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x85, 0x01,                    //     REPORT_ID (1)
-	0x05, 0x09,                    //     USAGE_PAGE (Button)
-	0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
-	0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
-	0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
-	0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-	0x95, 0x03,                    //     REPORT_COUNT (3)
-	0x75, 0x01,                    //     REPORT_SIZE (1)
-	0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-	0x95, 0x01,                    //     REPORT_COUNT (1)
-	0x75, 0x05,                    //     REPORT_SIZE (5)
-	0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
-	0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
-	0x09, 0x30,                    //     USAGE (X)
-	0x09, 0x31,                    //     USAGE (Y)
-	0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
-	0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
-	0x75, 0x08,                    //     REPORT_SIZE (8)
-	0x95, 0x02,                    //     REPORT_COUNT (2)
-	0x81, 0x06,                    //     INPUT (Data,Var,Rel)
-	0xc0,                          //   END_COLLECTION
-	0xc0                           // END_COLLECTION
-};
-*/
 
 __ALIGN_BEGIN static uint8_t HID_MOUSE_ReportDesc[HID_MOUSE_REPORT_DESC_SIZE]  __ALIGN_END = {
 	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
@@ -499,7 +416,28 @@ __ALIGN_BEGIN static uint8_t HID_MOUSE_ReportDesc[HID_MOUSE_REPORT_DESC_SIZE]  _
     0x81, 0x06,        //         INPUT (Data,Var,Rel)
 
 	0xc0,                          //   END_COLLECTION
-	0xc0                           // END_COLLECTION
+	0xc0,                           // END_COLLECTION
+
+    0x06, 0x00, 0xff,              // USAGE_PAGE (Vendor Defined Page 1)
+    0x09, 0x00,                    // USAGE (Undefined)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0xa1, 0x02,                    //   COLLECTION (Logical)
+    0x85, 0x02,                    //     REPORT_ID (2)
+    0x09, 0x00,                    //     USAGE (Undefined)
+    0x75, 0x20,                    //     REPORT_SIZE (32)
+    0x95, 0x04,                    //     REPORT_COUNT (4)
+    0x81, 0x82,                    //     INPUT (Data,Var,Abs,Vol)
+    0xc0,                          //   END_COLLECTION
+
+    0xa1, 0x02,                    //   COLLECTION (Logical)
+    0x85, 0x03,                    //     REPORT_ID (3)
+    0x09, 0x00,                    //     USAGE (Undefined)
+    0x75, 0x20,                    //     REPORT_SIZE (32)
+    0x95, 0x04,                    //     REPORT_COUNT (4)
+    0x91, 0x82,                    //     OUTPUT (Data,Var,Abs,Vol)
+    0xc0,                          //   END_COLLECTION
+    0xc0                           // END_COLLECTION
+
 };
 
 /**
