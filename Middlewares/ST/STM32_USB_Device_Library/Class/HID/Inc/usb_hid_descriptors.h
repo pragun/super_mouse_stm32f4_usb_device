05000e13fcc0afb6db8d953f351f7260d1736c63
Pragun <pragun.goyal@gmail.com>
2020-06-25 11:41:19 -0400

    Modified the Keyboard HID Descriptor to match what seems to be the std

--- Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc/usb_hid_descriptors.h
+++ Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc/usb_hid_descriptors.h
@@ -232,6 +232,15 @@ __ALIGN_BEGIN static uint8_t HID_MOUSE_ReportDesc[HID_MOUSE_REPORT_DESC_SIZE]  _
 	0xa1, 0x02,        //   COLLECTION (Logical)
 	0x85, 0x02,        //   Report ID (2)
 	0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
+
+	0x75, 0x01,        //   Report Size (1)
+	0x95, 0x08,        //   Report Count (8)
+	0x19, 0xE0,        //   Usage Minimum (0xE0)
+	0x29, 0xE7,        //   Usage Maximum (0xE7)
+	0x15, 0x00,        //   Logical Minimum (0)
+	0x25, 0x01,        //   Logical Maximum (1)
+	0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+
 	0x95, 0x04,        //   Report Count (4)
 	0x75, 0x08,        //   Report Size (8)
 	0x15, 0x00,        //   Logical Minimum (0)
