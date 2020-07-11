0e1e1e6452824b0cd1ebb1333f931397285c2045
pragun <pragun.goyal@gmail.com>
2020-07-10 20:44:02 -0400

    Refactored header for flash/ram node
    Use between storage on flash/ram is
    very nicely handled now IMHO

--- flash_key_value_tree/key_value_node.cpp
+++ flash_key_value_tree/key_value_node.cpp
@@ -1,37 +1,23 @@
 #include <key_value_node.hpp>
 
-extern bool flash_write_byte(void* address, uint8_t data);
+
+extern bool flash_write_byte(uint32_t flash_addr, uint8_t data);
 
 extern bool memcpy_to_flash(uint32_t flash_addr, uint8_t* data, uint8_t size);
 
 
-/*
-template<enum Validity_Flag_Enum flag>
-bool Key_Value_Ram_Node::flag_state(){
-	return (bool) (header.validity_bitmask & (0x1 << static_cast<uint8_t>(flag)));
-}
-
-template<>
-bool Key_Value_Ram_Node::flag_state<Validity_Flag_Enum::growth_node>(){
-	return (link_status(Implicit_Link_ID) == Link_State_Enum::uninitialized);
-	//return (bool) (header->validity_bitmask & (0x1 << static_cast<uint8_t>(flag)));
-}
-*/
-
-
 template<Validity_Flag_Enum flag>
-inline bool Node_Header_Typedef::flag_state() const{
+inline bool Node_Header_Data::flag_state() const{
 	return (bool) (validity_bitmask & (0x1 << static_cast<uint8_t>(flag)));
 }
 
-
 template<>
-inline bool Node_Header_Typedef::flag_state<Validity_Flag_Enum::growth_node>() const {
+inline bool Node_Header_Data::flag_state<Validity_Flag_Enum::growth_node>() const {
 	return (link_status(Implicit_Link_ID) == Link_State_Enum::uninitialized);
 	//return (bool) (header->validity_bitmask & (0x1 << static_cast<uint8_t>(flag)));
 }
 
-inline Link_State_Enum Node_Header_Typedef::link_status(uint8_t link_id) const {
+inline Link_State_Enum Node_Header_Data::link_status(uint8_t link_id) const {
 	uint8_t status = 0;
 	status = (node_link_status & (0b11 << (link_id*2))) >> (link_id*2);
 	return static_cast<Link_State_Enum>(status);
@@ -39,64 +25,57 @@ inline Link_State_Enum Node_Header_Typedef::link_status(uint8_t link_id) const {
 
 
 template<>
-inline bool Node_Header_Typedef::mark_link_as<Node_Storage::ram>(uint8_t link_id, Link_State_Enum link_state){
+inline bool Node_Header_Typedef<Storage::ram>::mark_link_as(uint8_t link_id, Link_State_Enum link_state){
 	uint8_t new_link_status = node_link_status & ~(0b11 << (link_id*2));
 	node_link_status |= (static_cast<uint8_t>(link_state)) << (link_id*2);
 	return true;
 }
 
 template<>
-inline bool Node_Header_Typedef::mark_link_as<Node_Storage::flash>(uint8_t link_id, Link_State_Enum link_state){
+inline bool Node_Header_Typedef<Storage::flash>::mark_link_as(uint8_t link_id, Link_State_Enum link_state){
 	uint8_t new_link_status = node_link_status & ~(0b11 << (link_id*2));
 	new_link_status |= (static_cast<uint8_t>(link_state)) << (link_id*2);
-	return flash_write_byte((void*) &(node_link_status), new_link_status);
+	return flash_write_byte((uint32_t) &(node_link_status), new_link_status);
 }
 
-/*
-template<Node_Storage NS,Validity_Flag_Enum flag>
-bool Node_Header_Typedef::invalidate_flag(){
-	uint8_t new_validity_byte = header->validity_bitmask & (0b1 << flag);
-	return flash_write_byte((void*) &(header->validity_bitmask), new_validity_byte);
+
+template<>
+template<Validity_Flag_Enum flag>
+inline bool Node_Header_Typedef<Storage::ram>::validate_flag(){
+	validity_bitmask |=  (0x1 << static_cast<uint8_t>(flag));
+	return true;
 }
 
 template<>
-bool Key_Value_Flash_Node::invalidate_flag<Validity_Flag_Enum::growth_node>();
-*/
+template<>
+inline bool Node_Header_Typedef<Storage::ram>::validate_flag<Validity_Flag_Enum::growth_node>(){
+	mark_link_as(Implicit_Link_ID, Link_State_Enum::uninitialized);
+	return true;
+}
+
+template<>
+template<Validity_Flag_Enum flag>
+bool Node_Header_Typedef<Storage::flash>::invalidate_flag(){
+	uint8_t new_validity_byte = validity_bitmask & (0b1 << flag);
+	return flash_write_byte((uint32_t) &(validity_bitmask), new_validity_byte);
+}
+
+template<>
+template<>
+bool Node_Header_Typedef<Storage::flash>::invalidate_flag<Validity_Flag_Enum::growth_node>();
+
+
 
 /** --- Key_Value_RAM_Node --- **/
 
-template<Validity_Flag_Enum flag>
-inline void Key_Value_Ram_Node::validate_flag(){
-	header.validity_bitmask |=  (0x1 << static_cast<uint8_t>(flag));
-}
-
-
-template<>
-inline void Key_Value_Ram_Node::validate_flag<Validity_Flag_Enum::growth_node>(){
-	header.mark_link_as<Node_Storage::ram>(Implicit_Link_ID, Link_State_Enum::uninitialized);
-}
-
-/*
-inline void Key_Value_Ram_Node::mark_link_by_index_as(uint8_t link_id, Link_State_Enum link_state){
-	uint8_t new_link_status = header.node_link_status & ~(0b11 << (link_id*2));
-	new_link_status |= (static_cast<uint8_t>(link_state)) << (link_id*2);
-	header.node_link_status = new_link_status;
-}
-*/
 
 Key_Value_Ram_Node::Key_Value_Ram_Node(uint32_t key, uint8_t size, uint8_t* data_ptr)
 {
-	header = {
-			.validity_bitmask = 0x0, //All flags invalid
-			.node_link_status = 0x0, //All links invalid
-			.key = key,
-			.value_size = size,
-			//.value
-	};
-
-	validate_flag<Validity_Flag_Enum::data_valid>();
-	validate_flag<Validity_Flag_Enum::growth_node>();
+	header_obj = {{ 0x0, 0x0, key, size}}; //The status byte and the node_link_status are initially all cleared out
+	header_obj.validate_flag<Validity_Flag_Enum::data_valid>();
+	header_obj.validate_flag<Validity_Flag_Enum::growth_node>();
 	data = data_ptr;
+	header = &header_obj;
 };
 
 inline Key_Value_Ram_Node Key_Value_Ram_Node::create_new_growth_node(uint32_t key, uint8_t size, uint8_t* data){
@@ -107,13 +86,13 @@ bool Key_Value_Ram_Node::write_to_address(Node_Address addr){
 	uint32_t offset = 0;
 	uint32_t write_addr = (uint32_t) addr;
 
-	memcpy_to_flash(write_addr, (uint8_t*) &header, sizeof(header));
-	write_addr += sizeof(header);
+	memcpy_to_flash(write_addr, (uint8_t*) &header_obj, sizeof(header_obj));
+	write_addr += sizeof(header_obj);
 
-	memcpy_to_flash(write_addr, data, header.value_size);
-	write_addr += header.value_size;
+	memcpy_to_flash(write_addr, data, header->value_size);
+	write_addr += header->value_size;
 
-	if(header.flag_state<Validity_Flag_Enum::root_node>()){
+	if(header->flag_state<Validity_Flag_Enum::root_node>()){
 		memcpy_to_flash(write_addr, (uint8_t*) &root_link_address, 4);
 		write_addr += 4;
 	}else{
@@ -121,7 +100,7 @@ bool Key_Value_Ram_Node::write_to_address(Node_Address addr){
 	}
 
 	for (uint8_t i = 1; i < MAX_NUM_CHILD_NODES; i++){ //The first link is implicit
-		if(header.link_status(i) == Link_State_Enum::valid){
+		if(header->link_status(i) == Link_State_Enum::valid){
 			memcpy_to_flash(write_addr, (uint8_t*) &link_addresses[i], 4);
 			write_addr += 4;
 		}
@@ -136,9 +115,9 @@ bool Key_Value_Ram_Node::write_to_address(Node_Address addr){
 
 Key_Value_Flash_Node::Key_Value_Flash_Node(Node_Address address):
 address_on_flash(address),
-header(reinterpret_cast<Node_Header_Typedef*>(address))
+header(reinterpret_cast<Node_Header_Typedef<Storage::flash>*>(address))
 {
-	uint8_t offset = sizeof(Node_Header_Typedef) + header->value_size;
+	uint8_t offset = sizeof(Node_Header_Typedef<Storage::flash>) + header->value_size;
 	uint8_t* byte_address = (uint8_t*) address;
 
 	if(header->flag_state<Validity_Flag_Enum::root_node>()){
@@ -209,65 +188,16 @@ inline bool Key_Value_Flash_Node::is_root() const{
 
 inline bool Key_Value_Flash_Node::mark_link_by_addr_as(Node_Address address, Link_State_Enum link_state){
 	uint8_t link_id = find_link_id_by_address(address);
-	return header->mark_link_as<Node_Storage::flash>(link_id, link_state);
+	return header->mark_link_as(link_id, link_state);
 }
 
+//This is a static function
 bool Key_Value_Flash_Node::growth_node_find_func(Key_Value_Flash_Node &a) {
 	return (a.header->flag_state<Validity_Flag_Enum::growth_node>());
 }
 
-/*
-inline bool Key_Value_Flash_Node::mark_link_by_index_as(uint8_t link_id, Link_State_Enum link_state){
-	uint8_t new_link_status = header->node_link_status & ~(0b11 << (link_id*2));
-	new_link_status |= (static_cast<uint8_t>(link_state)) << (link_id*2);
-	return flash_write_byte((void*) &(header->node_link_status), new_link_status);
-}*/
-
-/*
-template<enum Validity_Flag_Enum flag>
-bool Key_Value_Flash_Node::flag_state(){
-	return (bool) (header->validity_bitmask & (0x1 << static_cast<uint8_t>(flag)));
-}
-
-template<>
-bool Key_Value_Flash_Node::flag_state<Validity_Flag_Enum::growth_node>(){
-	return (link_status(Implicit_Link_ID) == Link_State_Enum::uninitialized);
-	//return (bool) (header->validity_bitmask & (0x1 << static_cast<uint8_t>(flag)));
-}
-*/
-
-/*
-template<enum Validity_Flag_Enum flag>
-bool Key_Value_Flash_Node::invalidate_flag(){
-	uint8_t new_validity_byte = header->validity_bitmask & (0b1 << flag);
-	return flash_write_byte((void*) &(header->validity_bitmask), new_validity_byte);
-}
-
-template<>
-bool Key_Value_Flash_Node::invalidate_flag<Validity_Flag_Enum::growth_node>();
-*/
-
-/*
-Link_State_Enum Key_Value_Flash_Node::link_status(uint8_t link_id){
-	uint8_t status = 0;
-	status = (header->node_link_status & (0b11 << (link_id*2))) >> (link_id*2);
-	return static_cast<Link_State_Enum>(status);
-}
-*/
-
-/*
- * There has not been any need for these yet
-bool Key_Value_Flash_Node::mark_link_as_invalid(Node_Address address){
-	return mark_link_by_addr_as(address, Link_State_Enum::invalid);
-}
-
-bool Key_Value_Flash_Node::mark_link_as_valid(Node_Address address){
-	return mark_link_by_addr_as(address, Link_State_Enum::valid);
-}
-*/
-
 bool Key_Value_Flash_Node::mark_implicit_link_as_valid(){
-	return header->mark_link_as<Node_Storage::flash>(Implicit_Link_ID, Link_State_Enum::valid);
+	return header->mark_link_as(Implicit_Link_ID, Link_State_Enum::valid);
 }
 
 Node_Address Key_Value_Flash_Node::next_root_node_link(){
@@ -290,10 +220,4 @@ Node_Address Key_Value_Flash_Node::add_growth_link(){
 	return (Node_Address) 0;
 }
 
-/*
-//This function is only used if the node is to be written to the flash
-Key_Value_Flash_Node Key_Value_Flash_Node::construct_for_writing_to_flash(uint8_t application_index, uint8_t keypad_keynum, uint8_t event_data_size, uint8_t* event_data){
-	return Key_Value_Flash_Node(0x0); // This is crap put in just to compile.
-}
-*/
 
--- flash_key_value_tree/key_value_node.hpp
+++ flash_key_value_tree/key_value_node.hpp
@@ -22,14 +22,14 @@ enum class Link_State_Enum: uint8_t{
 	invalid = 0b00,
 };
 
-enum class Node_Storage{
+enum class Storage{
 	ram, flash
 };
 
 using Node_Address = uint32_t;
 
 #pragma pack(1)
-struct Node_Header_Typedef{
+struct Node_Header_Data{
 	uint8_t validity_bitmask;
 	uint8_t node_link_status;
 
@@ -47,13 +47,20 @@ struct Node_Header_Typedef{
 
 	uint8_t value[]; //This makes sense only in the case of a node read from flash
 
-	Link_State_Enum link_status(uint8_t link_id) const;
 	template<Validity_Flag_Enum flag> bool flag_state() const;
-	template<Node_Storage> bool mark_link_as(uint8_t link_id, Link_State_Enum link_state);
+	Link_State_Enum link_status(uint8_t link_id) const;
 };
 
-static_assert(std::is_standard_layout<Node_Header_Typedef>::value);
-static_assert(std::is_standard_layout<Node_Header_Typedef>::value);
+#pragma pack(1)
+template <Storage NS>
+struct Node_Header_Typedef : Node_Header_Data{
+	bool mark_link_as(uint8_t link_id, Link_State_Enum link_state);
+	template<Validity_Flag_Enum flag> bool validate_flag();
+	template<Validity_Flag_Enum flag> bool invalidate_flag();
+};
+
+static_assert(std::is_standard_layout<Node_Header_Typedef<Storage::flash>>::value);
+static_assert(std::is_standard_layout<Node_Header_Typedef<Storage::ram>>::value);
 
 class Key_Value_Ram_Node{
 private:
@@ -63,14 +70,14 @@ private:
 	std::array<Node_Address, MAX_NUM_CHILD_NODES> link_addresses;
 	Node_Address implicit_link_address;
 	uint8_t* data;
-	Node_Header_Typedef header;
+	Node_Header_Typedef<Storage::ram> header_obj;
+	Node_Header_Typedef<Storage::ram>* header;
 	Key_Value_Ram_Node(uint32_t key, uint8_t size, uint8_t* value);
 
 public:
 	static Key_Value_Ram_Node create_new_growth_node(uint32_t key, uint8_t size, uint8_t* value);
 	bool write_to_address(Node_Address);
-	template<enum Validity_Flag_Enum flag> void validate_flag();
-	//void mark_link_by_index_as(uint8_t link_id, Link_State_Enum link_state);
+
 };
 
 class Key_Value_Flash_Node {
@@ -78,48 +85,30 @@ private:
 	Key_Value_Flash_Node(Node_Address address);
 	uint8_t find_link_id_by_address(Node_Address address);
 	bool mark_link_by_addr_as(Node_Address address, Link_State_Enum link_state);
-	//bool mark_link_by_index_as(uint8_t link_id, Link_State_Enum link_state);
 
 	Node_Address address_on_flash;
 	uint8_t size_on_flash;
 	Node_Address root_link_address;
-	//Node_Address* link_addresses;
 
 	std::array<Node_Address, MAX_NUM_CHILD_NODES> link_addresses;
 	Node_Address implicit_link_address;
 
-	Node_Header_Typedef* header;
+	Node_Header_Typedef<Storage::flash>* header;
 
 public:
+	static Key_Value_Flash_Node read_node_from_flash(Node_Address address);
 
-//	template<enum Validity_Flag_Enum flag> bool flag_state();
-//	template<enum Validity_Flag_Enum flag> bool invalidate_flag(); // do not use this use the corresponding function from header instead
-
-//	Link_State_Enum link_status(uint8_t link_id);
-//	bool mark_link_as_invalid(Node_Address address);
-//	bool mark_link_as_valid(Node_Address address);
-
-	bool mark_implicit_link_as_valid();
-
+	bool is_root() const;
 	Node_Address next_root_node_link();
 
 	std::array<Node_Address,MAX_NUM_CHILD_NODES> valid_children();
 
-	static Key_Value_Flash_Node read_node_from_flash(Node_Address address);
-
+	bool mark_implicit_link_as_valid();
 	Node_Address add_growth_link();
-
+	static bool growth_node_find_func(Key_Value_Flash_Node &a);
 	uint32_t furthest_memory_location();
 
 	uint32_t key() const;
-
 	uint8_t* value() const;
-
 	uint8_t value_size() const;
-
-	bool is_root() const;
-
-	static bool growth_node_find_func(Key_Value_Flash_Node &a);
-	//This function is only used if the node is to be written to the flash
-	//static Key_Value_Flash_Node construct_for_writing_to_flash(uint32_t key, uint8_t data_size, uint8_t* data);
 };
