9993c55812f94c70651c34d465edb0d20bbc9f7d
pragun <pragun.goyal@gmail.com>
2020-12-10 15:41:10 -0500

    Adds a simple serialization function to serialize one single configuration entry

--- rpc/rpc_impl.cpp
+++ rpc/rpc_impl.cpp
@@ -28,42 +28,50 @@ DEF_RPC_FUNC(ERASE_FLASH_SECTOR)(const uint8_t* buf)->void{
 
 }
 
-
 constexpr uint8_t CFG_ENTRY_SERIALIZATION_SIZE_LIMIT = 60;
 
 struct Cfg_Serialized_Entry_TypeDef{
 	uint8_t report_type;
 	uint8_t cfg_size;
-	std::array<uint8_t, CFG_ENTRY_SERIALIZATION_SIZE_LIMIT> data;
+	uint8_t data[CFG_ENTRY_SERIALIZATION_SIZE_LIMIT];
+};
+
+struct CFG_EVT_Serialized_TypeDef{
+	uint8_t size;
+	uint8_t data[CFG_ENTRY_SERIALIZATION_SIZE_LIMIT];
 };
 
 struct User_Config_TypeDef{
 	uint8_t application_id;
 	uint8_t keypad_key;
 
-	Cfg_Serialized_Entry_TypeDef key_down;
-	Cfg_Serialized_Entry_TypeDef mouse_movement;
-	Cfg_Serialized_Entry_TypeDef short_release;
-	Cfg_Serialized_Entry_TypeDef long_release;
+	CFG_EVT_Serialized_TypeDef key_down;
+	CFG_EVT_Serialized_TypeDef mouse_movement;
+	CFG_EVT_Serialized_TypeDef short_release;
+	CFG_EVT_Serialized_TypeDef long_release;
 };
 
 
 template <ReportingFunctionEnum report_type>
-Cfg_Serialized_Entry_TypeDef C(Reporting_Func_Params_Typedef<report_type> obj){
-	Cfg_Serialized_Entry_TypeDef a;
-	a.report_type = (uint8_t) report_type;
+CFG_EVT_Serialized_TypeDef
+C(Reporting_Func_Params_Typedef<report_type> obj){
+	CFG_EVT_Serialized_TypeDef a;
 	uint8_t siz = (uint8_t) sizeof(Reporting_Func_Params_Typedef<report_type>);
-	a.cfg_size = siz;
+
+	a.data[0] = (uint8_t) report_type;
+	a.data[1] = siz;
 
 	uint8_t* ptr = reinterpret_cast<uint8_t*>(&obj);
 	for (uint8_t i = 0; i < siz; i++) {
 		a.data[i + 2] = ptr[i];
 	}
 
+	a.size = siz + 2;
+
 	return a;
 }
 
-struct key_size_value_flash{
+struct Key_Size_Value_For_Flash{
 	uint32_t key;
 	uint8_t size;
 	uint8_t value[CFG_ENTRY_SERIALIZATION_SIZE_LIMIT];
@@ -89,8 +97,40 @@ User_Config_TypeDef list_of_config_key_values[] =
 */
 
 
-key_size_value_flash convert_cfg_entry_to_key_size_value(Cfg_Serialized_Entry_TypeDef cfg){
+Key_Size_Value_For_Flash convert_cfg_entry_to_key_size_value(User_Config_TypeDef cfg){
+	Key_Size_Value_For_Flash ksvf_obj;
+	ksvf_obj.key = (cfg.application_id << 8) | (cfg.keypad_key);
 
+	constexpr uint8_t evt_table_length = 4;
+	uint8_t evt_offset_table[evt_table_length];
+
+	evt_offset_table[0] = evt_table_length;
+	evt_offset_table[1] = evt_offset_table[0] + cfg.key_down.size;
+	evt_offset_table[2] = evt_offset_table[1] + cfg.mouse_movement.size;
+	evt_offset_table[3] = evt_offset_table[2] + cfg.short_release.size;
+	ksvf_obj.size = evt_offset_table[3] + cfg.long_release.size;
+
+	for (uint8_t i = 0; i<evt_table_length; i++){
+		ksvf_obj.value[i] = evt_offset_table[i];
+	}
+
+	for (uint8_t i = 0; i<cfg.key_down.size; i++){
+		ksvf_obj.value[i + evt_offset_table[0]] = cfg.key_down.data[i];
+	}
+
+	for (uint8_t i = 0; i<cfg.mouse_movement.size; i++){
+		ksvf_obj.value[i + evt_offset_table[1]] = cfg.mouse_movement.data[i];
+	}
+
+	for (uint8_t i = 0; i<cfg.short_release.size; i++){
+		ksvf_obj.value[i + evt_offset_table[2]] = cfg.key_down.data[i];
+	}
+
+	for (uint8_t i = 0; i<cfg.long_release.size; i++){
+		ksvf_obj.value[i + evt_offset_table[3]] = cfg.long_release.data[i];
+	}
+
+	return ksvf_obj;
 }
 
 
